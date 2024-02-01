3748af9eb81e43ab3ccf7bc4331a2a490fc85294
Tanish Shah <connecttanish@gmail.com>
2024-01-31 17:46:43 -0800

    updated main script

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ from document_tagger import DocumentTagger
 import spacy
 import sys
 
-def main(document_filepath: str, en_model_name: str):
+def main(document_filepath: str, organization_tags: str, en_model_name: str):
     nlp_model = spacy.load(name=en_model_name)  
     doc_parser = DocumentParser(document_filepath=document_filepath)
     doc_tagger = DocumentTagger(parser=doc_parser, 
-                                organization_tags=['porn', 'hentai', 'anime', 'machine learning', 'society', 'language', 'artificial intelligence', 'data', 'company'],
+                                organization_tags=[tag.strip().lower() for tag in organization_tags.split(',')],
                                 nlp_model=nlp_model) 
 
-    print(doc_tagger.fetch_autotags())
+    suggested_auto_tags = doc_tagger.fetch_autotags()
+    for suggested_auto_tag in suggested_auto_tags:
+        print(suggested_auto_tag)
+
 
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
